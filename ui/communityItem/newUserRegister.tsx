f746a60a4bfaee9619ac29ea1e89b98bbf97f2fe
nyanshiba1 <nyanshiba77@gmail.com>
2023-07-14 06:05:54 +0900

    コミュニティのユーザー登録

---
+++ ui/communityItem/newUserRegister.tsx
@@ -0,0 +1,25 @@
+'use client';
+
+import styled from 'styled-components';
+
+
+export default function NewUserRegister() {
+  return (
+    <>
+      <form className="m-auto w-80">
+       
+          <div className="flex justify-center items-center">
+            <figure className="h-32 w-32 rounded-full bg-gray-300"style={{ marginTop: '30px' }}>
+              <div className="h-full w-full rounded-full border-4 border-gradient-to-br from-red-500 via-yellow-400 to-blue-500"></div>
+            </figure>
+
+
+   
+        </div>
+
+ 
+        
+      </form>
+    </>
+  );
+}
