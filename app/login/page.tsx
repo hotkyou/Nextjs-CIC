bb4541ba583773f4b79320c6f374c3138b5b1c61
hotkyou <hottarakasi.stu@gmail.com>
2023-07-14 03:14:44 +0900

    add: login

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,23 +1,69 @@
-import { Button_link } from '@/ui/button/button';
+'use client';
+import { Button } from '@/ui/button/button';
 import { Input_L, Label } from '@/ui/createAccount/formItem';
+import { useState } from 'react';
+
+
 
 export default function Login() {
+  const [formData, setFormData] = useState({ mail: '', password: '' })
+
+  const data = {
+
+    mail: formData.mail,
+    password: formData.password,
+  };
+
+  const sendFormData = async () => {
+    try {
+      const response = await fetch('http://localhost:3000/login', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(data)
+      });
+
+      if (response.ok) {
+        const responseData = await response.json();
+        // リクエストが成功した場合の処理
+        console.log('送信おk サーバーからのリスポンス：', responseData);
+        // リクエストが成功した場合の処理
+        console.log('データが正常に送信されました');
+      } else {
+        // リクエストが失敗した場合の処理
+        console.error('データの送信に失敗しました');
+      }
+    } catch (error) {
+      // エラーハンドリング
+      console.error('通信エラー:', error);
+    }
+  };
+  const handleSubmit = (e: { preventDefault: () => void; }) => {
+    e.preventDefault();
+    console.log(formData);
+    sendFormData();
+  };
+
+
+  
   return (
     <>
-      <form className="mx-auto mt-6 flex w-60 flex-col items-center">
+      <form  onSubmit={handleSubmit} className="mx-auto mt-6 flex w-60 flex-col items-center">
         <div className="mb-6 w-full">
           <Label>電話番号</Label>
-          <Input_L type="tel" name="tel" placeholder="電話番号" />
+          <Input_L type="tel" name="tel" value={formData.mail} onChange={(e: { target: { value: any; }; }) => setFormData({ ...formData, mail: e.target.value })}  key="tel-input" placeholder="電話番号" />
         </div>
 
         <div className="mb-6 w-full">
           <Label>パスワード</Label>
           <div>
-            <Input_L type="password" name="password" placeholder="パスワード" />
+            <Input_L type="password" value={formData.password}name="password" onChange={(e: { target: { value: any; }; }) => setFormData({ ...formData, password: e.target.value })}  key="password-input"  placeholder="パスワード" />
           </div>
         </div>
-        <Button_link href="/comm">ログイン</Button_link>
+        <Button type ="submit">ログイン</Button>
       </form>
     </>
   );
-}
+
+  }
